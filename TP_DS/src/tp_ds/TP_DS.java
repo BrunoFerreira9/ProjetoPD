849085a055b21c6122b39bddafc19a50050eeefe
joaojsimoes <45096382+joaojsimoes@users.noreply.github.com>
2019-10-23 20:34:40 +0100

    ResolveMessages feito

--- TP_DS/src/tp_ds/TP_DS.java
+++ TP_DS/src/tp_ds/TP_DS.java
@@ -5,12 +5,18 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TP_DS {
 
     public static void main(String[] args){
+        HashMap <String,String> message1 = ResolveMessages("tipo | login ; username | pd_user ; password | pd_1234");
+        HashMap <String,String> message2 =ResolveMessages("tipo | resposta ; sucesso | sim ; msg | Autenticação realizada com sucesso.");
+        System.out.println(message1);
+        System.out.println(message2);
         ComunicacaoToCliente ComToCli = new ComunicacaoToCliente();
         ComunicacaoToServidor ComToSer = new ComunicacaoToServidor();
         byte []info = new byte[128];
@@ -22,19 +28,27 @@ public class TP_DS {
             pkt = new DatagramPacket(info,info.length);
             while(true){
                 sock.receive(pkt);
-                
-                System.out.println("Recebi "+new String(pkt.getData(),0,pkt.getLength()).toString()+" de "+pkt.getAddress()+" porto: "+pkt.getPort());
+                System.out.println("Recebi "+new String(pkt.getData(),0,pkt.getLength())+" de "+pkt.getAddress()+" porto: "+pkt.getPort());
             }
         }catch(SocketException e) {
             System.out.println ("Error "+e);
         } catch (IOException ex) {
             Logger.getLogger(TP_DS.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        } 
+       
     }
     
-    public static void ResolveMessages(String message){
-        
+    public static HashMap<String,String> ResolveMessages(String message){
+        StringTokenizer t,tokens = new StringTokenizer(message,";");
+        String key,val;
+        HashMap<String,String> messages = new HashMap<>();
+        while (tokens.hasMoreElements()) {
+            t = new StringTokenizer(tokens.nextElement().toString()," | ");  
+            key = t.nextElement().toString();
+            val = t.nextElement().toString();
+            messages.put(key, val);
+        }
+        return messages;
     }
     
 }
